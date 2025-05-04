db04939cdf048d39d41ec8c02f539b0fb07f3041
amirmtaati <amirmtaati@gmail.com>
2025-05-04 20:47:54 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Amir's Modern Emacs Configuration
+
+![Screenshot of Emacs with this config](screenshot.png) *[optional screenshot]*
+
+A **literate-programming** Emacs configuration that transforms your editor into a:
+- Full-featured IDE ✨
+- Knowledge management system 📚
+- Productivity powerhouse ⚡
+
+## 🚀 Installation
+
+1. Clone this repo to your config directory:
+
+```bash
+git clone https://github.com/yourusername/emacs-config.git ~/.config/emacs
---
+++ init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file "~/.config/emacs/emacs.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ceebbe6bf99d69b22440db9ef27b22032e12d2a48be291531757891e74c853c0" "87064601426c5d29cebbf6c7a2ccd2fdbfef1ae8f4aeffe9b085f5ea86aa61ff" "8d71b6dec68d21ba814421349d437e8b9745106dcdd0b2c67fad7670f17e1e61" "52632b69c2813771327a2c22f51ccaca466ba3cc1aa8f3bf2d613573ea934993" "6b912e025527ffae0feb76217f1a3e494b0699e5219ab59ea4b3a36c319cea17" default))
+ '(ispell-dictionary nil)
+ '(package-selected-packages
+   '(wordcount-section org-modern org-appear obsidian exec-path-from-shell jupyter ob-ipython org-roam-ui slime gorepl-mode go-mode org-present langtool ox-hugo rainbow-delimiters sly pdf-tools which-key vterm all-the-icons-ivy all-the-icons-ivy-rich xelb visual-fill-column use-package python-mode org-roam org-bullets olivetti modus-themes lsp-ui ivy-rich ivy-prescient focus evil-surround evil-org evil-collection elpy doom-modeline default-text-scale dashboard counsel-projectile company-box))
+ '(warning-suppress-log-types '((modus-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
